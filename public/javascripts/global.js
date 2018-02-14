e2ab8e9314971644fdb4f0af093d0ce9c17d9250
Ekkalak Leelasornchai <ekkalakfm@gmail.com>
2018-02-14 21:52:07 +0700

    Fix dashboard

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,5 +1,5 @@
 // Userlist data array for filling in info box
-var userListData = [];
+var coworkingListData = [];
 
 // DOM Ready =============================================================
 $(document).ready(function() {
@@ -8,13 +8,14 @@ $(document).ready(function() {
     populateTable();
 
     // Username link click
-    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+    $('#coworkingList table tbody').on('click', 'td a.linkshowuser', showCoworkingInfo);
 
     // Add User button click
-    $('#btnAddUser').on('click', addUser);
+    $('#btnAddCoworking').on('click', addCoworking);
+    // $('#btnAddCoworking').on('click', editCoworking);
 
     // Delete User link click
-    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+    $('#coworkingList table tbody').on('click', 'td a.linkdeleteuser', deleteCoworking);
 
 });
 
@@ -30,52 +31,52 @@ function populateTable() {
     $.getJSON( '/coworking/coworkinglist/', function( data ) {
 
         // Stick our user data array into a userlist variable in the global object
-        userListData = data;
+        coworkingListData = data;
 
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '" title="Show Details">' + this.username + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.name + '" title="Show Details">' + this.name + '</a></td>';
+            tableContent += '<td>' + this.img + '</td>';
+            tableContent += '<td>' + this.space + '</td>';
             tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
         });
 
         // Inject the whole content string into our existing HTML table
-        $('#userList table tbody').html(tableContent);
+        $('#coworkingList table tbody').html(tableContent);
     });
 };
 
 // Show User Info
-function showUserInfo(event) {
+function showCoworkingInfo(event) {
 
     // Prevent Link from Firing
     event.preventDefault();
 
     // Retrieve username from link rel attribute
-    var thisUserName = $(this).attr('rel');
+    var thisCoworkingName = $(this).attr('rel');
 
     // Get Index of object based on id value
-    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+    var arrayPosition = coworkingListData.map(function(arrayItem) { return arrayItem.name; }).indexOf(thisCoworkingName);
 
     // Get our User Object
-    var thisUserObject = userListData[arrayPosition];
+    var thisCoworkingObject = coworkingListData[arrayPosition];
 
     //Populate Info Box
-    $('#userInfoName').text(thisUserObject.fullname);
-    $('#userInfoAge').text(thisUserObject.age);
-    $('#userInfoGender').text(thisUserObject.gender);
-    $('#userInfoLocation').text(thisUserObject.location);
+    $('#coworkingInfoName').text(thisCoworkingObject.name);
+    $('#coworkingInfoImg').text(thisCoworkingObject.img);
+    $('#coworkingInfoSpace').text(thisCoworkingObject.space);
 
 };
 
-// Add User
-function addUser(event) {
+// Add Coworking
+function addCoworking(event) {
     event.preventDefault();
 
     // Super basic validation - increase errorCount variable if any fields are blank
     var errorCount = 0;
-    $('#addUser input').each(function(index, val) {
+    $('#addCoworking input').each(function(index, val) {
         if($(this).val() === '') { errorCount++; }
     });
 
@@ -83,19 +84,16 @@ function addUser(event) {
     if(errorCount === 0) {
 
         // If it is, compile all user info into one object
-        var newUser = {
-            'username': $('#addUser fieldset input#inputUserName').val(),
-            'email': $('#addUser fieldset input#inputUserEmail').val(),
-            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
-            'age': $('#addUser fieldset input#inputUserAge').val(),
-            'location': $('#addUser fieldset input#inputUserLocation').val(),
-            'gender': $('#addUser fieldset input#inputUserGender').val()
+        var newCoworking = {
+            'name': $('#addCoworking fieldset input#inputCoworkingName').val(),
+            'img': $('#addCoworking fieldset input#inputCoworkingImg').val(),
+            'space': $('#addCoworking fieldset input#inputCoworkingSpace').val()
         }
 
         // Use AJAX to post the object to our adduser service
         $.ajax({
             type: 'POST',
-            data: newUser,
+            data: newCoworking,
             url: '/coworking/addcoworking/',
             dataType: 'JSON'
         }).done(function( response ) {
@@ -104,7 +102,7 @@ function addUser(event) {
             if (response.msg === '') {
 
                 // Clear the form inputs
-                $('#addUser fieldset input').val('');
+                $('#addCoworking fieldset input').val('');
 
                 // Update the table
                 populateTable();
@@ -126,7 +124,7 @@ function addUser(event) {
 };
 
 // Delete User
-function deleteUser(event) {
+function deleteCoworking(event) {
 
     event.preventDefault();
 
@@ -162,4 +160,57 @@ function deleteUser(event) {
 
     }
 
+};
+
+//Edit Coworking
+function editCoworking(event) {
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#editCoworking input').each(function(index, val) {
+        if($(this).val() === '') { errorCount++; }
+    });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        // If it is, compile all user info into one object
+        var newCoworking = {
+            'name': $('#addCoworking fieldset input#inputCoworkingName').val(),
+            'img': $('#addCoworking fieldset input#inputCoworkingImg').val(),
+            'space': $('#addCoworking fieldset input#inputCoworkingSpace').val()
+        }
+
+        // Use AJAX to post the object to our adduser service
+        $.ajax({
+            type: 'PUT',
+            data: newCoworking,
+            url: '/coworking/addcoworking/',
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response
+            if (response.msg === '') {
+
+                // Clear the form inputs
+                $('#addCoworking fieldset input').val('');
+
+                // Update the table
+                populateTable();
+
+            }
+            else {
+
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
 };
